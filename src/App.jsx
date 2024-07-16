91fcbe0b2f99fb2e379edc5530669a22be174742
npnam2k3 <nguyenphuongnam284@gmail.com>
2024-07-16 16:40:33 +0700

    dark light mode p2

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,52 @@ import Typography from "@mui/material/Typography";
 
 import { useColorScheme } from "@mui/material/styles";
 
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Box from "@mui/material/Box";
+
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutLined";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme();
+  const handleChange = (event) => {
+    setMode(event.target.value);
+  };
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="label-select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="dark">
+          <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <DarkModeOutlinedIcon fontSize="small" /> Dark
+          </div>
+        </MenuItem>
+        <MenuItem value="light">
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <LightModeIcon fontSize="small" /> Light
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <SettingsBrightnessIcon fontSize="small" /> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
+
 function ModeToggle() {
   const { mode, setMode } = useColorScheme();
   return (
@@ -23,6 +69,8 @@ function ModeToggle() {
 function App() {
   return (
     <>
+      <ModeSelect />
+      <br />
       <ModeToggle />
       <div>Devil dev</div>
       <Typography variant="body2" color="text.secondary">
