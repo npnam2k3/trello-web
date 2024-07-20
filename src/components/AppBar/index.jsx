1efdc36656ffbdef4ac25c7671be6f0e65e224e4
npnam2k3 <nguyenphuongnam284@gmail.com>
2024-07-20 09:00:35 +0700

    code app bar p2

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -33,6 +33,7 @@ function AppBar() {
         <Box sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
           <SvgIcon
             component={TrelloIcon}
+            fontSize="small"
             inheritViewBox
             sx={{ color: "primary.main" }}
           />
@@ -61,13 +62,13 @@ function AppBar() {
           size="small"
         />
         <ModeSelect />
-        <Tooltip title="Notification">
+        <Tooltip title="Notifications">
           <Badge color="secondary" variant="dot" sx={{ cursor: "pointer" }}>
-            <NotificationsNoneIcon />
+            <NotificationsNoneIcon sx={{ color: "primary.main" }} />
           </Badge>
         </Tooltip>
         <Tooltip title="Help">
-          <HelpOutlineIcon sx={{ cursor: "pointer" }} />
+          <HelpOutlineIcon sx={{ cursor: "pointer", color: "primary.main" }} />
         </Tooltip>
         <Profiles />
       </Box>
