aa0b78e2aeefe18c6793ea7e3d008b28de1c89f1
npnam2k3 <nguyenphuongnam284@gmail.com>
2024-07-21 21:41:02 +0700

    refactor structure folder

--- src/pages/Boards/BoardBar/BoardBar.jsx
+++ src/pages/Boards/BoardBar/BoardBar.jsx
@@ -38,7 +38,6 @@ function BoardBar() {
         overflowX: "auto",
         bgcolor: (theme) =>
           theme.palette.mode === "dark" ? "#34495e" : "#1976d2",
-        borderBottom: "1px solid #00bfa5",
         "&::-webkit-scrollbar-track": { m: 2 },
       }}
     >
