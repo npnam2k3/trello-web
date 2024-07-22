d26d90f9d23f6c10eaa185adb75b8dc4a7de5e75
npnam2k3 <nguyenphuongnam284@gmail.com>
2024-07-22 09:45:31 +0700

    mock data and sort data

--- src/utils/sorts.js
+++ src/utils/sorts.js
@@ -1,1 +1,8 @@
 //sort column/cards
+export const mapOrder = (originArr, orderArr, key) => {
+  const cloneArr = [...originArr];
+  const orderedArr = cloneArr.sort((a, b) => {
+    return orderArr.indexOf(a[key]) - orderArr.indexOf(b[key]);
+  });
+  return orderedArr;
+};
